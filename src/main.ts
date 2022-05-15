17579dfe78e1ea097b225e98a52f2ea26983c894
Demetriy <ArhDemetriy@GMail.com>
2022-05-15 18:09:58 +0800

    fix PORT

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const PORT = process?.env?.PORT || 5000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, function (...args) { console.log([this].concat(args)) });
+  await app.listen(PORT, function (...args) { console.log([this].concat(args)) });
 }
 bootstrap();
