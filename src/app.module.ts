ebd8fdb7c01d365bfcbfb05d2b3620fb5a5650a3
Demetriy <ArhDemetriy@GMail.com>
2022-05-15 18:11:17 +0800

    init DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      // host: 'localhost',
+      host: 'postgresql-horizontal-62466',
+      port: 5432,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      logging: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
