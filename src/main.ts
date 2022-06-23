f3c343a68f51a378f104dfc9e0e95c90102a1f66
Demetriy <ArhDemetriy@GMail.com>
2022-06-23 18:45:00 +0800

    try add ps

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const PORT = process?.env?.PORT || 5000
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(PORT, function (...args) { console.log([this].concat(args)) });
+async function bootstrap()
+{
+  const app = NestFactory.create(AppModule)
+  const PORT = process?.env?.PORT || 5000
+  return (await app).listen(PORT, function (...args) { console.log('bootstrap; listen callback', [this].concat(args)) })
 }
+
 bootstrap();
